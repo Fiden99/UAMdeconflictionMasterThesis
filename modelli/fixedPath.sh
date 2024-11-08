4cf18b96b42ae03e44d04e82af5a126001b8c1d7
Filippo <filippo99magi@gmail.com>
2024-11-08 17:06:46 +0100

    fixedPath

--- modelli/fixedPath.sh
+++ modelli/fixedPath.sh
@@ -4,21 +4,6 @@
 #SBATCH --error=/home/magi/UAMdeconflictionMasterThesis/modelli/out/fixed/ampl_error_%A_%a.txt    # File di output per stderr
 #SBATCH --ntasks=1                      # Numero di task per job
 #SBATCH --cpus-per-task=1               # Numero di core per job
-<<<<<<< HEAD
-#SBATCH --array=0-59
-
-# Crea una lista dei nomi dei file e dei tempi dal file "moreIterations"
-declare -A FILES_AND_TIMES
-while read -r line; do
-    name=$(echo "$line" | cut -d ' ' -f 1)
-    time=$(echo "$line" | cut -d ' ' -f 2)
-    FILES_AND_TIMES["$name"]=$time
-done < /home/magi/UAMdeconflictionMasterThesis/modelli/moreIterations.txt
-
-# Trova i file .dat nella directory "data/" che corrispondono ai nomi in "moreIterations"
-FILES=()
-TIMES=()
-=======
 #SBATCH --array=0-30                    # Numero di job array
 
 # Crea una lista dei nomi dei file, tempi, numero di nodi e voli dal file "fixedPathFiles.txt"
@@ -46,21 +31,17 @@ TIMES=()
 NODES=()
 FLIGHTS=()
 
->>>>>>> e927e4750db0f3a3c8716bbc806944918ac50b4b
 for file in /home/magi/UAMdeconflictionMasterThesis/modelli/data/*.dat; do
     filename=$(basename "$file" .dat)
     if [[ -n "${FILES_AND_TIMES[$filename]}" ]]; then
         FILES+=("$file")
         TIMES+=("${FILES_AND_TIMES[$filename]}")
-<<<<<<< HEAD
-=======
         IFS=' ' read -r node flight <<< "${NODES_AND_FLIGHTS[$filename]}"
         NODES+=("$node")
         FLIGHTS+=("$flight")
         
         # Debug: stampa il file trovato
         echo "File .dat trovato e associato: $filename"
->>>>>>> e927e4750db0f3a3c8716bbc806944918ac50b4b
     fi
 done
 
@@ -76,25 +57,15 @@ fi
 # Carica il modulo AMPL (se necessario, se AMPL è gestito tramite moduli)
 # module load ampl
 
-<<<<<<< HEAD
-# Seleziona il file e il tempo corrispondente all'indice dell'array di Slurm
-datFile=${FILES[$SLURM_ARRAY_TASK_ID]}
-timeLimit=${TIMES[$SLURM_ARRAY_TASK_ID]}
-=======
 # Seleziona il file, il tempo, il numero di nodi e di voli corrispondente all'indice dell'array di Slurm
 datFile=${FILES[$SLURM_ARRAY_TASK_ID]}
 timeLimit=${TIMES[$SLURM_ARRAY_TASK_ID]}
 nNodes=${NODES[$SLURM_ARRAY_TASK_ID]}
 nFlights=${FLIGHTS[$SLURM_ARRAY_TASK_ID]}
->>>>>>> e927e4750db0f3a3c8716bbc806944918ac50b4b
 
 # Ottieni il percorso assoluto del file corrente
 datFileBase=$(basename "$datFile" .dat)
 
-<<<<<<< HEAD
-# Esegui AMPL con il file .dat corrente e il tempo massimo
-timeLimit="$timeLimit" nNodes="10" nFlights="100" absPath=$PWD datFile=$datFileBase ampl /home/magi/UAMdeconflictionMasterThesis/modelli/fixedPath.run
-=======
 # Debug: stampa i parametri prima dell'esecuzione
 echo "Esecuzione AMPL con i seguenti parametri:"
 echo "File: $datFile"
@@ -106,4 +77,3 @@ echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
 
 # Esegui AMPL con il file .dat corrente e i parametri specificati
 timeLimit="$timeLimit" nNodes="$nNodes" nFlights="$nFlights" absPath=$PWD datFile=$datFileBase ampl /home/magi/UAMdeconflictionMasterThesis/modelli/fixedPath.run
->>>>>>> e927e4750db0f3a3c8716bbc806944918ac50b4b
