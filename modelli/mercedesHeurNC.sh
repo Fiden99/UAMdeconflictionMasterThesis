d337b936bd9c804579203500bd08a237f901e12c
Filippo Magi <filippo99magi@gmail.com>
2025-03-04 22:55:40 +0100

    update heuristic

---
+++ modelli/mercedesHeurNC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=MercedesInstances     # Nome del job
+#SBATCH --output=/home/magi/UAMdeconflictionMasterThesis/modelli/out/mercedesHeur/ampl_output_%A_%a.txt  # File di output per stdout (%A = job ID, %a = array task ID)
+#SBATCH --error=/home/magi/UAMdeconflictionMasterThesis/modelli/out/mercedesHeur/ampl_error_%A_%a.txt    # File di output per stderr
+#SBATCH --ntasks=1                      # Numero di task per job
+#SBATCH --cpus-per-task=1               # Numero di core per job
+#SBATCH --time=96:00:00                 # Tempo massimo per ogni job 
+#SBATCH --array=0-300%20                # Numero di job da eseguire
+
+# Crea una lista di tutti i file .dat nella directory "data/"
+FILES=($(ls /home/magi/UAMdeconflictionMasterThesis/modelli/data/mercedesTDHeur/NC/*.dat))
+# FILES=(/home/magi/UAMdeconflictionMasterThesis/modelli/data/*.dat)
+
+# Seleziona il file corrispondente all'indice dell'array di Slurm
+datFile=${FILES[$SLURM_ARRAY_TASK_ID]}
+
+# Ottieni il percorso assoluto del file corrente
+datFileBase=$(basename "$datFile" .dat)
+
+# Esegui AMPL con il file .dat corrente
+absPath=$PWD datFile=$datFileBase ampl /home/magi/UAMdeconflictionMasterThesis/modelli/UAM_heur_run/mercedesHeuristicSbatchNC.run
